fbee3399aa90d225131e6d1f6e2e56c3e9d5d1fc
Takeru Ohta <phjgt308@gmail.com>
2012-02-01 10:54:39 +0900

    instruction: mov

--- cl-asm.lisp
+++ cl-asm.lisp
@@ -19,7 +19,7 @@
   (name t :type symbol)
   (addr 0 :type address))
 
-(defstruct (imm (:include destination))
+(defstruct (imm (:include operand))
   (value 0 :type integer))
 
 (defstruct (mem (:include destination)))
@@ -49,18 +49,6 @@
      (setf (sb-alien:deref codes i) (elt ,seq i))
      FINALLY
      (return (sb-alien:cast codes ,function-type-spec))))
-#|
-
-pushq %rbp
-movq %rsp, %rbp
-movl %edi, -4(%rbp)
-movl %esi, -8(%rbp)
-movl -8(%rbp), %eax
-movl -4(%rbp), %edx
-addl %edx, %eax
-popq %rbp
-ret
-|#
 
 (defun flatten (x)
   (typecase x
@@ -69,202 +57,24 @@ ret
     (cons (destructuring-bind (car . cdr) x
             (append (flatten car) (flatten cdr))))))
 
-(defun get-rd (sym)
-  (ecase (intern (symbol-name sym) :cl-asm)
-    ((%eax %rax) 0)
-    ((%ecx %rcx) 1)
-    ((%edx %rdx) 2)
-    ((%ebx %rbx) 3)
-    ((%esp %rsp) 4)
-    ((%ebp %rbp) 5)
-    ((%esi %rsi) 6)
-    ((%edi %rdi) 7)))
-
-#+C
-(defun reg-code (sym)
-  (ecase (intern (symbol-name sym) :cl-asm)
-    ((%al %ax %eax %rax) 0)
-    ((%cl %cx %ecx %rcx) 1)
-    ((%dl %dx %edx %rdx) 2)
-    ((%bl %bx %ebx %rbx) 3)
-    ((%ah %sp %esp %rsp) 4)
-    ((%ch %bp %ebp %rbp) 5)
-    ((%dh %si %esi %rsi) 6)
-    ((%bh %di %edi %rdi) 7)))
-
-(defun @push (arg)
-  (assert (= 1 (length arg)))
-  (etypecase #1=(car arg)
-    (symbol (+ #x50 (get-rd #1#)))
-    (integer `(#x68 ,@(int32-to-bytes #1#)))))
-
-(defun @pop (arg)
-  (if (= (length arg) 1)
-      (+ #x58 (get-rd (car arg)))
-    (error "unsupported")))
-
 (defparameter +rex.w+ #x48)
-(defparameter +mod.3+ #b11)
-
-(defun mk-r/r-modrm (from to)
-  (+ (ash +mod.3+       6)
-     (ash (get-rd from) 3)
-     (ash (get-rd to)   0)))
- 
-(defun mk-m/r-modrm (base to-reg)
-  (+ (ash #b01            6)
-     (ash (get-rd to-reg) 3)
-     (ash (get-rd base)   0)))
-
-#+C
-(defun reg-p (sym)
-  (and (symbolp sym)
-       (member sym '(%rax %rcx %rdx %rbx %rsp %rbp %rsi %rdi
-                     %eax %ecx %edx %ebx %esp %ebp %esi %edi) :test #'string=)))
-                 
-(defun r/r-p (arg)
-  (and (= (length arg) 2)
-       (reg-p (first arg)) 
-       (reg-p (second arg))))
-
-(defun m/r-p (arg)
-  (and (= (length arg) 2)
-       (consp (first arg))
-       (reg-p (second arg))))
-
-(defun i/r-p (arg)
-  (and (= (length arg) 2)
-       (integerp (first arg))
-       (reg-p (second arg))))
-
-(defun i/eax-p (arg)
-  (and (= (length arg) 2)
-       (integerp (first arg))
-       (member (second arg) '(%eax %ax %al) :test #'string=)))
-  
-(defun to-ubyte (n)
-  (ldb (byte 8 0) n))
-
-(defun reg-type-of (reg)
-  (ecase (intern (symbol-name reg) :cl-asm)
-    ((%al %cl %bl %bl %ah %ch %dh %bh)         :r8)
-    ((%ax %cx %dx %bx %sp %bp %si %di)         :r16)
-    ((%eax %ecx %edx %ebx %esp %ebp %esi %edi) :r32)
-    ((%rax %rcx %rdx %rbx %rsp %rbp %rsi %rdi) :r64)))
-
-(defun operand-type-of (operand)
-  (etypecase operand
-    (integer 
-     (let ((len (integer-length operand)))
-       (cond ((< len 08) :imm8)
-             ((< len 16) :imm16)
-             ((< len 32) :imm32)
-             ((< len 64) :imm64)
-             (t (error "unsupported")))))
-    (symbol 
-     (if (char/= #\% (char (symbol-name operand) 0))
-         :address
-       (reg-type-of operand)))
-    (cons 
-     (destructuring-bind (tag a &optional b) operand
-       (ecase tag
-         (:ref
-          (if (and (null b)
-                   (integerp a)
-                   (>= (integer-length a) 32))
-              :m64
-            :m))
-         ((:imm8 :imm16 :imm32 :imm64) tag))))))
-
-(defun modrm (mod r/m reg)
-  (+ (ash mod 6)
-     (ash reg 3)
-     (ash r/m 0)))
-
-;; TODO: r/m-modrmと統合可能
-#+C
-(defun r/r-modrm (from to)
-  (modrm #b11 (reg-code to) (reg-code from)))
-
 (defparameter +sib.none+ #x25)
-
-#+C
-(defun r/m-modrm (from to)
-  (destructuring-bind (tag a1 &optional a2) to
-    (declare (ignore tag))
-    (etypecase a1
-      (integer 
-       (assert (null a2))
-       `(,(modrm #b00 #b100 (reg-code from)) ,+sib.none+ ,@(int32-to-bytes a1)))
-      (symbol
-       (if (null a2)
-           (let ((cd (reg-code a1)))
-             (assert (member cd '(0 1 2 3 6 7)))
-             (modrm #b00 cd (reg-code from)))
-         (let ((cd (reg-code a1)))
-           (assert (member cd '(0 1 2 3 4 6 7)))
-           (ecase (operand-type-of a2)
-             (:imm8 `(,(modrm #b01 cd (reg-code from)) ,(to-ubyte a2)))
-             ((:imm16 :imm32) `(,(modrm #b10 cd (reg-code from)) ,@(int32-to-bytes a2)))))))
-      )))
-
-(defun mk-modrm (from to)
-  (if (and (symbolp from) (symbolp to))
-      (r/r-modrm from to)
-    (if (symbolp from)
-        (r/m-modrm from to)
-      (r/m-modrm to from))))
-     
-(defun reg-a-p (reg)
-  (member reg '(%al %ax %eax %rax) :test #'string=))
 
 (defun int-to-bytes (width n)
   (loop FOR i FROM 0 BELOW width
         COLLECT (ldb (byte 8 (* i 8)) n)))
-
-;; TODO
-(defun mk-mov-op (base opd &optional (offset 1))
-  (ecase (operand-type-of opd)
-    ((:r8 :imm8) `(,base))
-    ((:r16 :imm16) `(#x66 ,(+ base offset)))
-    ((:r32 :imm32) `(,(+ base offset)))
-    ((:r64 :imm64) `(,+rex.w+ ,(+ base offset)))))
 
 (defmacro def-ins (name args &body body)
   (let ((arg (gensym)))
     `(defun ,name (,arg)
        ;; TODO: error message
        (destructuring-bind ,args ,arg
-         (flatten (locally ,@body))))))
+         (flatten (locally (to-list ,@body)))))))
 
-
-#|
-(case x
-  (:reg->xxx)
-  (:mem->xxx)
-  (:imm->xxx)
-  (:reg->mem64)
-  (:mem64->xxx))
-
-(def-ins @mov (from to)
-  (ecase (bin-type-of from to)
-    (:reg->xxx 
-     `(,(mk-mov-op #x88 from) ,(mk-modrm from to)))
-    (:mem->xxx 
-     `(,(mk-mov-op #x8B to) ,(mk-modrm from to)))
-    (:imm->xxx 
-     (ecase (operand-type-of to)
-       (:m `(,(mk-mov-op #xC6 from) ,(mk-modrm '%ax to) ,(int-to-bytes len n))) ; TODO: len:max4
-       (:reg `(,(mk-mov-op (+ #xb0 (reg-code to)) to 8) ,(int-to-bytes len from))) ; TODO: len
-       ))
-    (:reg->mem64 
-     `(,(mk-mov-op #xA2 from) ,@(int-to-bytes 8 (second to))))
-    (:mem64->xxx
-     `(,(mk-mov-op #xA0 to) ,(int-to-bytes 8 (second from))))))
-|#
-
+;; XXX: 分かりにくい名前
 (defun mem64-p (operand)
-  (and (mem-p operand) (= (mem-size operand) 8)))
+  (and (mem-direct-p operand)
+       (> (integer-length (mem-direct-addr operand)) 32)))
 
 (defun r/m-p (operand)
   (or (reg-p operand) (mem-p operand)))
@@ -272,19 +82,48 @@ ret
 (defun make-mod-r/m (mod reg/opcode r/m)
   (+ (ash mod 6) (ash reg/opcode 3) r/m))
 
+(defun make-x/m-mod-r/m (op/reg-code mem)
+  (etypecase mem
+    (mem-direct 
+     (list (make-mod-r/m #b00 op/reg-code #b100)
+           +SIB.NONE+ (int-to-bytes 4 (mem-direct-addr mem))))
+    (mem-indirect
+     (let ((mem-off (mem-indirect-offset mem))
+           (mem-reg-code (reg-code (mem-indirect-reg mem))))
+     (if (zerop mem-off)
+         (make-mod-r/m #b00 op/reg-code mem-reg-code)
+       (multiple-value-bind (mod addr-size)
+                            (ecase (imm-byte-width mem-off)
+                              (1     (values #b01 1))
+                              ((2 4) (values #b10 4)))
+         (list (make-mod-r/m mod op/reg-code mem-reg-code) (int-to-bytes addr-size mem-off))))))))
+
+(defun make-r/m-mod-r/m (reg mem)
+  (make-x/m-mod-r/m (reg-code reg) mem))
+
+(defun make-o/m-mod-r/m (opcode mem)
+  (make-x/m-mod-r/m opcode mem))
+
 (defun mod-r/m (reg/opcode r/m)
-  (flet ((mod-type-of (a b)
-           (cond ((and (reg-p a) (reg-p b)) :r/r)
+  (flet ((mod-type-of (r/o r/m)
+           (cond ((and (integerp r/o) (mem-p r/m)) :o/m)
+                 ((and (reg-p r/o) (reg-p r/m)) :r/r)
+                 ((and (reg-p r/o) (mem-p r/m)) :r/m)
                  (t (error "unsupported")))))
     (ecase (mod-type-of reg/opcode r/m)
       (:r/r (make-mod-r/m #b11 (reg-code reg/opcode) (reg-code r/m)))
+      (:r/m (make-r/m-mod-r/m reg/opcode r/m))
+      (:o/m (make-o/m-mod-r/m reg/opcode r/m))
       )))
 
+(defun reg@a-p (operand)
+  (and (reg-p operand)
+       (= 0 (reg-code operand))))
 
 (def-ins @mov (dst src)
   (flet ((operands-type-of (dst src)
-           (cond ((and (r/m-p dst) (mem64-p src)) :r/m<-mem64)
-                 ((and (mem64-p dst) (reg-p src)) :mem64<-reg)
+           (cond ((and (mem64-p dst) (reg@a-p src)) :mem64<-reg@a)
+                 ((and (reg@a-p dst) (mem64-p src)) :reg@a<-mem64)
                  ((and (r/m-p dst) (reg-p src))   :r/m<-reg)
                  ((and (r/m-p dst) (mem-p src))   :r/m<-mem)
                  ((and (mem-p dst) (imm-p src))   :mem<-imm)
@@ -298,70 +137,23 @@ ret
              (1 base)
              (2 (list #x66 (+ base offset)))
              (4 (+ base offset))
-             (8 (list +REX.W+ (+ base offset))))))
+             (8 (list +REX.W+ (+ base offset)))))
+
+         (disp (n &optional (size (destination-size dst)))
+           (int-to-bytes size n)))
 
     (ecase (operands-type-of dst src)
       (:r/m<-reg (list (op #x88) (mod-r/m src dst)))
-      (:r/m<-mem )
-      (:mem<-imm )
-      (:reg<-imm )
-      (:mem64<-reg )
-      (:r/m<-mem64 ))))
+      (:r/m<-mem (list (op #x8B) (mod-r/m dst src)))
+      (:reg<-imm (list (op (+ #xB0 (reg-code dst)) 8) (disp (imm-value src))))
+      (:mem<-imm (list (op #xC6) (mod-r/m 0 dst) (disp (imm-value src))))
+      (:mem64<-reg@a (list (op #xA2) (disp (mem-direct-addr dst) 8))) ; XXX: 32bit対応
+      (:reg@a<-mem64 (list (op #xA0) (disp (mem-direct-addr src) 8))) ; XXX: 32bit対応
+      )))
 
-(defun @add (arg)
-  (cond ((r/r-p arg)
-         `(#x01 ,(mk-r/r-modrm (first arg) (second arg))))
-        ((i/eax-p arg)
-         (let ((len (integer-length (first arg))))
-           (cond ((< len  8) `(#x04 ,(to-ubyte (first arg))))
-                 (t (error "unsupported")))))
-        (t
-         (error "unsupported"))))
-
-(defun @sub (arg)
-  (cond ((r/r-p arg)
-         `(#x29 ,(mk-r/r-modrm (first arg) (second arg))))
-        (t
-         (error "unsupported"))))
-
-(defun @cmp (arg)
-  (cond ((r/r-p arg)
-         `(#x39 ,(mk-r/r-modrm (first arg) (second arg))))
-        (t
-         (error "unsupported"))))
-
-(defun @ret (arg)
-  (declare (ignore arg))
+;; TODO: extend
+(def-ins @ret ()
   #xC3)
-
-(defun int32-to-bytes (n)
-  (loop FOR i FROM 0 BELOW 4
-        COLLECT (ldb (byte 8 (* 8 i)) n)))
-
-(defun @jmp-unresolve ()
-  `(#xE9 0 0 0 0))
-
-(defun @jmp (arg)
-  (assert (and (= (length arg) 1)
-               (integerp (first arg))))
-  `(#xE9 ,@(int32-to-bytes (first arg)))) ; relative 32bit jump
-
-(defun jmp-cond (cond)
-  (ecase cond
-    (:<  #x77)
-    (:<= #x73)
-    (:>  #x72)
-    (:>= #x76)
-    (:=  #x74)
-    (:zero #x74) ; XXX
-    (:/= #x75)
-    ))
-
-(defun @jmp-if-unresolve (arg)
-  `(,(jmp-cond (first arg)) 0)) ; relative 8bit jump
-
-(defun @jmp-if (arg)
-  `(,(jmp-cond (first arg)) ,(second arg)))
 
 (defun to-list (x) (if (listp x) x (list x)))
 
@@ -380,9 +172,6 @@ ret
        (let ((ptr (extern-fn-ptr name)))
          (append (@mov `(,ptr %eax)) ; TODO: 64bit mov
                  `(#xFF ,#b11010000))))))) ; /2 = 010
-
-(defun @call-unresolve ()
-  `(,#xE8 0 0 0 0))
 
 #+C
 (defun assemble (mnemonics)
@@ -452,7 +241,7 @@ ret
            
 (defun ref-imm-p (args)
   (and (= 1 (length args))
-       (integerp args)))
+       (integerp (first args))))
 
 (defun ref-extern-p (args &aux (fst (first args)))
   (and (= 1 (length args))
@@ -472,7 +261,7 @@ ret
 
 (defun to-operand (operand)
   (etypecase operand
-    (integer (make-imm :size (imm-byte-width operand) :value operand))
+    (integer (make-imm :value operand))
     (symbol  (cond ((operand-label-p operand)
                     (make-label :name operand))
                    ((operand-register-p operand)
@@ -492,7 +281,8 @@ ret
                    (make-mem-direct :size size :addr (extern-address (second (first args)))))
                   ((ref-register-p args)
                    (destructuring-bind (reg &optional (offset 0)) args
-                     (make-mem-indirect :size size :reg reg :offset offset)))
+                     ;; TODO: check size of register
+                     (make-mem-indirect :size size :reg (to-operand reg) :offset offset)))
                   (t
                    (error "unsupported"))))))))))
 
